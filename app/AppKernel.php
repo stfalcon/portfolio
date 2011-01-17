a705dd81bcad668e1e8df6cb6a4d5e0f3873e8c9
Stepan Tanasiychuk <ceo@stfalcon.com>
2011-01-17 21:54:46 +0200

    delete config samples and enable DoctrineMigrations Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\ZendBundle\ZendBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-            //new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
+            new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
             //new Symfony\Bundle\DoctrineMongoDBBundle\DoctrineMongoDBBundle(),
 
             // register your bundles
@@ -47,14 +47,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        // use YAML for configuration
-        // comment to use another configuration format
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-
-        // uncomment to use XML for configuration
-        //$loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.xml');
-
-        // uncomment to use PHP for configuration
-        //$loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.php');
     }
 }
