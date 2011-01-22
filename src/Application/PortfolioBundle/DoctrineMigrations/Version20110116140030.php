ef564449043bac6ed97910117479a968832a5111
Stepan Tanasiychuk <ceo@stfalcon.com>
2011-01-22 22:40:44 +0200

    rename projects table

--- src/Application/PortfolioBundle/DoctrineMigrations/Version20110116140030.php
+++ src/Application/PortfolioBundle/DoctrineMigrations/Version20110116140030.php
@@ -9,11 +9,11 @@ class Version20110116140030 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->_addSql("CREATE TABLE projects (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) ENGINE = InnoDB");
+        $this->_addSql("CREATE TABLE portfolio_projects (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) ENGINE = InnoDB");
     }
 
     public function down(Schema $schema)
     {
-        $this->_addSql("DROP TABLE projects");
+        $this->_addSql("DROP TABLE portfolio_projects");
     }
 }
