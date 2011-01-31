f99a74bd0e4fbd22b1eff67e3813398f62ef47ef
Stepan Tanasiychuk <ceo@stfalcon.com>
2011-01-31 14:36:34 +0200

    update sandbox. updated front controllers

--- web/app.php
+++ web/app.php
@@ -5,4 +5,4 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('prod', false);
-$kernel->handle(new Request())->send();
+$kernel->handle(Request::createFromGlobals())->send();
--- web/app_dev.php
+++ web/app_dev.php
@@ -11,4 +11,4 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
-$kernel->handle(new Request())->send();
+$kernel->handle(Request::createFromGlobals())->send();
