6eecd9b950d0b08143a5f731c5a5c489b8d2bed2
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2011-04-26 17:17:39 +0300

    change https to http

--- bin/vendors.sh
+++ bin/vendors.sh
@@ -19,7 +19,7 @@ mkdir -p "$VENDOR" && cd "$VENDOR"
 
 ##
 # @param destination directory (e.g. "doctrine")
-# @param URL of the git remote (e.g. https://github.com/doctrine/doctrine2.git)
+# @param URL of the git remote (e.g. http://github.com/doctrine/doctrine2.git)
 # @param revision to point the head (e.g. origin/HEAD)
 #
 install_git()
@@ -46,92 +46,92 @@ install_git()
 }
 
 # Assetic
-install_git assetic https://github.com/kriswallsmith/assetic.git
+install_git assetic http://github.com/kriswallsmith/assetic.git
 
 # Symfony
-install_git symfony https://github.com/symfony/symfony.git
+install_git symfony http://github.com/symfony/symfony.git
 
 # Doctrine ORM
-install_git doctrine https://github.com/doctrine/doctrine2.git
+install_git doctrine http://github.com/doctrine/doctrine2.git
 
 # Doctrine Data Fixtures Extension
-install_git doctrine-data-fixtures https://github.com/doctrine/data-fixtures.git
+install_git doctrine-data-fixtures http://github.com/doctrine/data-fixtures.git
 
 # Doctrine DBAL
-install_git doctrine-dbal https://github.com/doctrine/dbal.git
+install_git doctrine-dbal http://github.com/doctrine/dbal.git
 
 # Doctrine Common
-install_git doctrine-common https://github.com/doctrine/common.git
+install_git doctrine-common http://github.com/doctrine/common.git
 
 # Doctrine migrations
-install_git doctrine-migrations https://github.com/doctrine/migrations.git
+install_git doctrine-migrations http://github.com/doctrine/migrations.git
 
 # Doctrine MongoDB
-#install_git doctrine-mongodb https://github.com/doctrine/mongodb.git
+#install_git doctrine-mongodb http://github.com/doctrine/mongodb.git
 
 # Doctrine MongoDB
-#install_git doctrine-mongodb-odm https://github.com/doctrine/mongodb-odm.git
+#install_git doctrine-mongodb-odm http://github.com/doctrine/mongodb-odm.git
 
 # Swiftmailer
-install_git swiftmailer https://github.com/swiftmailer/swiftmailer.git origin/4.1
+install_git swiftmailer http://github.com/swiftmailer/swiftmailer.git origin/4.1
 
 # Twig
-install_git twig https://github.com/fabpot/Twig.git v1.0.0
+install_git twig http://github.com/fabpot/Twig.git v1.0.0
 
 # Twig Extensions
-install_git twig-extensions https://github.com/fabpot/Twig-extensions.git
+install_git twig-extensions http://github.com/fabpot/Twig-extensions.git
 
 # Monolog
 install_git monolog http://github.com/Seldaek/monolog.git
 
 # Imagine
-install_git imagine https://github.com/avalanche123/Imagine.git
+install_git imagine http://github.com/avalanche123/Imagine.git
 
 # Doctrine Extensions
-install_git doctrine-extensions https://github.com/l3pp4rd/DoctrineExtensions.git
+install_git doctrine-extensions http://github.com/l3pp4rd/DoctrineExtensions.git
 
 # ZF
 mkdir -p $VENDOR/zf/library
 svn co http://framework.zend.com/svn/framework/standard/tags/release-1.11.5/library/ $VENDOR/zf/library
 
 # ZF2
-install_git zf2 https://github.com/zendframework/zf2.git
+install_git zf2 http://github.com/zendframework/zf2.git
 
 
 # FunctionalTestBundle
 mkdir -p $BUNDLES/Liip
 cd $BUNDLES/Liip
-install_git FunctionalTestBundle https://github.com/liip/FunctionalTestBundle.git
+install_git FunctionalTestBundle http://github.com/liip/FunctionalTestBundle.git
 
 # MenuBundle
 mkdir -p $BUNDLES/Knplabs/Bundle
 cd $BUNDLES/Knplabs/Bundle
-install_git MenuBundle https://github.com/knplabs/MenuBundle.git
+install_git MenuBundle http://github.com/knplabs/MenuBundle.git
 
 # ZendCacheBundle
 mkdir -p $BUNDLES/Bundle
 cd $BUNDLES/Bundle
-install_git ZendCacheBundle https://github.com/knplabs/ZendCacheBundle.git
+install_git ZendCacheBundle http://github.com/knplabs/ZendCacheBundle.git
 
 # SensioFrameworkExtraBundle
 mkdir -p $BUNDLES/Sensio/Bundle
 cd $BUNDLES/Sensio/Bundle
-install_git FrameworkExtraBundle https://github.com/sensio/SensioFrameworkExtraBundle.git
+install_git FrameworkExtraBundle http://github.com/sensio/SensioFrameworkExtraBundle.git
 
 # SecurityExtraBundle
 mkdir -p $BUNDLES/JMS
 cd $BUNDLES/JMS
-install_git SecurityExtraBundle https://github.com/schmittjoh/SecurityExtraBundle.git
+install_git SecurityExtraBundle http://github.com/schmittjoh/SecurityExtraBundle.git
 
 # DoctrineExtensionsBundle
 mkdir -p $BUNDLES/Stof
 cd $BUNDLES/Stof
-install_git DoctrineExtensionsBundle https://github.com/stof/DoctrineExtensionsBundle.git
+install_git DoctrineExtensionsBundle http://github.com/stof/DoctrineExtensionsBundle.git
 
 # DoctrineFixturesBundle
 mkdir -p $VENDOR/symfony/src/Symfony/Bundle/
 cd $VENDOR/symfony/src/Symfony/Bundle/
-install_git DoctrineFixturesBundle https://github.com/symfony/DoctrineFixturesBundle.git
+install_git DoctrineFixturesBundle http://github.com/symfony/DoctrineFixturesBundle.git
 
 # Update the bootstrap files
 $DIR/bin/build_bootstrap.php
