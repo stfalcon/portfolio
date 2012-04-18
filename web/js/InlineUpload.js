1345a72ca15999255d090b5e108ad727f3f01111
emeraldMaster <constantine.emeraldmaster@gmail.com>
2012-04-18 18:19:30 +0300

    web/js/InlineUpload.js refactored

--- web/js/InlineUpload.js
+++ web/js/InlineUpload.js
@@ -1,34 +1,35 @@
 var InlineUpload = {
     dialog: null,
-    block: '',
     options: {
         form_class: 'inline_upload_form',
         action: '/posts/upload',
         iframe: 'inline_upload_iframe'
     },
     display: function(hash) {
-        pointer = this;
+        var self = this;
 
-        // Create invisible form and iframe
-        this.dialog = $([
-            '<div style="opacity:0;position:absolute;" class="inline_upload_container"><form class="',this.options.form_class,'" action="',this.options.action,'" target="',this.options.iframe,'" method="post" enctype="multipart/form-data">',
-            '<input name="form[inlineUploadFile]" type="file" /></form>' +
-            '<iframe id="',this.options.iframe,'" name="',this.options.iframe,'" class="',this.options.iframe,'" src="about:blank" width="0" height="0"></iframe></div>',
-        ].join(''));
-        if ($(document).find(".inline_upload_container").length == 0) {
+        this.dialog = $(document).find(".inline_upload_container");
+
+        if (!this.dialog.size()) {
+            // Create invisible form and iframe
+            this.dialog = $([
+                '<div style="opacity:0;position:absolute;" class="inline_upload_container"><form class="',this.options.form_class,'" action="',this.options.action,'" target="',this.options.iframe,'" method="post" enctype="multipart/form-data">',
+                '<input name="form[inlineUploadFile]" type="file" /></form>' +
+                '<iframe id="',this.options.iframe,'" name="',this.options.iframe,'" class="',this.options.iframe,'" src="about:blank" width="0" height="0"></iframe></div>',
+            ].join(''));
             this.dialog.appendTo(document.body);
-        } else {
-            this.dialog = $(document).find(".inline_upload_container");
         }
 
         // make 'click' action on file element right after 'Picture' selection on markItUp menu
         // to show system dialog
         $("input[name='form[inlineUploadFile]']").focus();
-        $("input[name='form[inlineUploadFile]']").click();
+        $("input[name='form[inlineUploadFile]']").trigger('click');
 
         // submit hidden form after file was selected in system dialog
         $("input[name='form[inlineUploadFile]']").live('change', function(){
-            if ($(this).val() != '') $('.' + pointer.options.form_class).submit();
+            if ($(this).val() != '') {
+                $('.' + self.options.form_class).submit();
+            }
         });
 
         // response will be sent to the hidden iframe
@@ -36,12 +37,12 @@ var InlineUpload = {
             if ($(this).contents().find('body').html() != '') {
                 var response = $.parseJSON($(this).contents().find('body').html());
                 if (response.status == 'success') {
-                    this.block = ['<img src="' + response.src + '" width="' + response.width + '" height="' + response.height + '" alt="" class=""/>'];
-                    $.markItUp({replaceWith: this.block.join('')} );
+                    var block = ['<img src="' + response.src + '" width="' + response.width + '" height="' + response.height + '" alt="" class=""/>'];
+                    $.markItUp({replaceWith: block.join('')} );
                 } else {
                     alert(response.msg);
                 }
-                pointer.cleanUp();
+                self.cleanUp();
             }
         });
     },
