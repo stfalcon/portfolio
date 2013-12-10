bebc90f19b6356be61ce56ace79135d9ead64386
vatseek <vatseek13@rambler.ru>
2013-12-10 10:28:37 +0200

    design home page

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -42,6 +42,7 @@ class AppKernel extends Kernel
             new Stfalcon\Bundle\PortfolioBundle\StfalconPortfolioBundle(),
 
             new Application\Bundle\DefaultBundle\ApplicationDefaultBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
