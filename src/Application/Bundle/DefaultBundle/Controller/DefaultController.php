1a04f300d47860651ea97c40ed3b7be59879f4eb
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2014-11-17 09:44:13 +0200

    Add actions

--- src/Application/Bundle/DefaultBundle/Controller/DefaultController.php
+++ src/Application/Bundle/DefaultBundle/Controller/DefaultController.php
@@ -6,7 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Application\Bundle\DefaultBundle\Form\Type\PromotionOrderFormType;
 /**
  * Default controller. For single actions for project
  *
@@ -49,4 +52,71 @@ class DefaultController extends Controller
         return array();
     }
 
+    /**
+     * Promotions apps page
+     *
+     * @Route("/promotion/apps", name="page_promotion_apps")
+     */
+    public function promotionAppsAction()
+    {
+        $form = $this->createForm(new PromotionOrderFormType());
+
+        return $this->render(
+            '@ApplicationDefault/Default/promotion_apps.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/order/apps", name="order_apps")
+     */
+    public function orderAppsAction(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            throw $this->createNotFoundException('Not supported');
+        }
+
+        $form = $this->createForm(new PromotionOrderFormType());
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            // Get base template for email
+            $templateContent = $this->get('twig')->loadTemplate(
+                'ApplicationDefaultBundle:emails:order_app.html.twig'
+            );
+
+            $body = $templateContent->render(
+                [
+                    'message' => $data['message'],
+                    'name'    => $data['name'],
+                    'email'   => $data['email']
+                ]
+            );
+
+            $mailer_from = $this->get('service_container')->getParameter('mailer_from');
+            $mailer_name = $this->get('service_container')->getParameter('mailer_name');
+            $mailer_notify = $this->get('service_container')->getParameter('mailer_notify');
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('')
+                ->setFrom($mailer_from, $mailer_name)
+                ->setTo($mailer_notify)
+                ->setBody($body, 'text/html');
+
+            $mailer = $this->get('mailer');
+            if ($mailer->send($message)) {
+                return new JsonResponse(['status' => 'success']);
+            } else {
+                return new JsonResponse(['status' => 'error']);
+            }
+        } else {
+            return new JsonResponse(['status' => 'error']);
+        }
+    }
 }
