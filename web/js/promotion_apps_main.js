b4e764664fa2e78e0ae4b7e496786b0b4dcda426
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2014-11-21 16:57:46 +0200

    Change constrain for form

--- web/js/promotion_apps_main.js
+++ web/js/promotion_apps_main.js
@@ -22,28 +22,34 @@ $(document).ready(function () {
         rules: {
             'order_promotion[name]': {
                 required: true,
-                minlength: 3
+                minlength: 3,
+                maxlength: 64
             },
             'order_promotion[email]': {
                 required: true,
-                minlength: 3
+                minlength: 3,
+                maxlength: 72
             },
             'order_promotion[message]': {
                 required: true,
-                minlength: 30
+                minlength: 30,
+                maxlength: 5000
             }
         },
         messages: {
             'order_promotion[name]': {
                 required: "Пожалуйста, введите ваше имя",
+                maxlength: "Пожалуйста, введите не более 64 символов.",
                 minlength: jQuery.validator.format("Введите имя не меньше 3 символов")
             },
             'order_promotion[email]': {
                 required: "Пожалуйста, введите адрес вашей эл.почты",
+                maxlength: "Пожалуйста, введите не более 72 символов.",
                 email: "Ваша эл.адрес должен быть формата name@domain.com"
             },
             'order_promotion[message]': {
                 required: "Пожалуйста, введите сообщение",
+                maxlength: "Пожалуйста, введите не более 5000 символов.",
                 minlength: "Пожалуйста, введите не менее 30 символов."
             }
         },
@@ -65,6 +71,8 @@ $(document).ready(function () {
                 },
                 success: function (response) {
                     if (response.status == "success") {
+                        ga('send', 'pageview', 'order_sent');
+
                         $('#feedback-form').find('.form-pad').animate({opacity: 0}, 300).delay(formDelay).animate({opacity: 1}, 300);
                         $('#feedback-form').find('.form-success').fadeIn(300).delay(formDelay).fadeOut(300);
                         $(form).trigger('reset');
