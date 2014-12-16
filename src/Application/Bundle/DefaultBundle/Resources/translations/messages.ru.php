be5fda363af4f7944b14f8edd10fe6566e5fc717
Alexandr Lensky <olexandr.lensky@stfalcon.com>
2014-12-16 17:39:55 +0200

    Added translations for design and dev landings

--- src/Application/Bundle/DefaultBundle/Resources/translations/messages.ru.php
+++ src/Application/Bundle/DefaultBundle/Resources/translations/messages.ru.php
@@ -3,5 +3,5 @@
 return array(
     'promotion.order.app.mail.subject' => 'Заявка на разработку мобильного приложения от "%email%"',
     'promotion.order.design.mail.subject' => 'Заявка на разработку веб дизайна от "%email%"',
-    'promotion.order.development.mail.subject' => 'Заявка на разработку веб разработки от "%email%"',
+    'promotion.order.development.mail.subject' => 'Заявка на веб разработки от "%email%"',
 );
