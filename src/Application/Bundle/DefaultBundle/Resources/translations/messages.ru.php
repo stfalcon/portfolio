dc8105f49e9b36586aa2ae1552335d069429ad25
Alexandr Lensky <olexandr.lensky@stfalcon.com>
2014-12-17 12:17:41 +0200

    Added sysadmin and mobile games landings

--- src/Application/Bundle/DefaultBundle/Resources/translations/messages.ru.php
+++ src/Application/Bundle/DefaultBundle/Resources/translations/messages.ru.php
@@ -4,4 +4,6 @@ return array(
     'promotion.order.app.mail.subject' => 'Заявка на разработку мобильного приложения от "%email%"',
     'promotion.order.design.mail.subject' => 'Заявка на разработку веб дизайна от "%email%"',
     'promotion.order.development.mail.subject' => 'Заявка на веб разработки от "%email%"',
+    'promotion.order.sysadmin.mail.subject' => 'Заявка на системное админимстрирование от "%email%"',
+    'promotion.order.mobilegames.mail.subject' => 'Заявка на разработку мобильной игры от "%email%"',
 );
