a9999ce2e4d2321725041bb1b4db0ff7f5915d75
Alexandr Lensky <olexandr.lensky@stfalcon.com>
2015-02-03 10:14:56 +0200

    Added for landings form validation errors for de locale

---
+++ web/js/locales/order_application_form.de.js
@@ -0,0 +1,20 @@
+var orderApplicationFormMessages = {
+    'order_promotion[name]': {
+        required: "Bitte den Namen eingeben",
+        maxlength: "Bitte geben Sie maximal 64 Zeichen",
+        minlength: jQuery.validator.format("Bitte geben Sie mindestens 3 Zeichen")
+    },
+    'order_promotion[email]': {
+        required: "Bitte gültige Emailadresse eingeben",
+        maxlength: "Bitte geben Sie maximal 72 Zeichen",
+        email: 'Geben Sie Ihre E-Mail-Adresse, beispielsweise "name@domain.com"'
+    },
+    'order_promotion[message]': {
+        required: "Bitte Ihre Mitteilung eingeben",
+        maxlength: "Bitte geben Sie maximal 5000 Zeichen",
+        minlength: "Bitte geben Sie mindestens 30 Zeichen"
+    }
+};
+
+
+
