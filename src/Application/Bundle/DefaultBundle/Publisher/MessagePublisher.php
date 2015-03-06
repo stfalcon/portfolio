9ccee88662173e050ac92b4ce56319d0adfe4b4e
Alexandr Lensky <olexandr.lensky@stfalcon.com>
2015-03-06 10:40:07 +0200

    Added greylog

---
+++ src/Application/Bundle/DefaultBundle/Publisher/MessagePublisher.php
@@ -0,0 +1,38 @@
+<?php
+namespace Application\Bundle\DefaultBundle\Publisher;
+
+use Gelf\Message;
+use Gelf\MessagePublisher as BasePublisher;
+
+/**
+ * Class MessagePublisher send messages to Graylog server
+ */
+class MessagePublisher extends BasePublisher
+{
+    /** @var  string */
+    private $fromName;
+
+    /**
+     * @param string $fromName
+     * @param string $hostname
+     * @param int    $port
+     * @param int    $chunkSize
+     */
+    public function __construct($fromName, $hostname, $port = self::GRAYLOG2_DEFAULT_PORT, $chunkSize = self::CHUNK_SIZE_WAN)
+    {
+        parent::__construct($hostname, $port, $chunkSize);
+        $this->fromName = $fromName;
+    }
+
+    /**
+     * @param Message $message
+     *
+     * @return bool
+     */
+    public function publish(Message $message)
+    {
+        $message->setHost($this->fromName);
+
+        return parent::publish($message);
+    }
+} 
