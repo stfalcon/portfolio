e47dd7bc3d93af7303a4e4b2a401d7d88193074d
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2015-05-06 15:57:04 +0300

    Add homepage

--- src/Application/Bundle/DefaultBundle/Service/SitemapService.php
+++ src/Application/Bundle/DefaultBundle/Service/SitemapService.php
@@ -45,6 +45,14 @@ class SitemapService
 
         $xmlSitemap = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" />');
 
+        $this->addUrlElement(
+            $xmlSitemap,
+            $this->router->generate('homepage', ['_locale' => $lang], true),
+            null,
+            'monthly',
+            1
+        );
+
         $this->addPortfolioItems($xmlSitemap, $lang);
         $this->addBlogItems($xmlSitemap, $lang);
         $this->addBlogTags($xmlSitemap, $lang);
