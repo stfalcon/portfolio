c2a81bfbbd0c960d9ad8a16458674b5c019dfe3f
Alexandr Lensky <olexandr.lensky@stfalcon.com>
2015-09-24 17:40:42 +0300

    Fix slider team page

--- web/js/main.js
+++ web/js/main.js
@@ -50,7 +50,7 @@ $(function () {
         $('#view-year').html(value)
     }
 
-    if($('#yeat-slider').length){
+    if($('#year-slider').length){
         $("#year-slider").slider({
             min: 0,
             max: yearList.length - 1,
