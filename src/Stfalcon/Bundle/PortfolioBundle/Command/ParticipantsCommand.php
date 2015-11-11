d0025971f00abe2dea18040a1fdc8079b6724470
LogansUA <logansoleg@gmail.com>
2015-11-11 12:42:42 +0200

    Add console command to move participants to another table

---
+++ src/Stfalcon/Bundle/PortfolioBundle/Command/ParticipantsCommand.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Stfalcon\Bundle\PortfolioBundle\Command;
+
+use Application\Bundle\UserBundle\Entity\User;
+use Stfalcon\Bundle\PortfolioBundle\Entity\Project;
+use Stfalcon\Bundle\PortfolioBundle\Entity\UserWithPosition;
+use Stfalcon\Bundle\PortfolioBundle\Repository\UserWithPositionRepository;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * ParticipantsCommand class
+ *
+ * @author Oleg Kachinsky <logansoleg@gmail.com>
+ */
+class ParticipantsCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('portfolio:project:participants')
+            ->setDescription('Move project participants to another table')
+            ->setHelp(<<<'HELP'
+The <info>%command.name%</info> move project participants to table where users have custom positions.
+HELP
+            );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        $projectRepository          = $em->getRepository('StfalconPortfolioBundle:Project');
+        $userWithPositionRepository = $em->getRepository('StfalconPortfolioBundle:UserWithPosition');
+
+        $projects = $projectRepository->findAllWithoutUserWithPosition();
+        if (empty($project)) {
+            foreach ($projects as $project) {
+                $participants = $project->getParticipants();
+
+                if (!empty($participants->getValues())) {
+                    /** @var User $participant */
+                    foreach ($participants as $participant) {
+                        /** @var UserWithPosition $userWithPosition */
+                        $userWithPosition = $this->getUserWithPosition($userWithPositionRepository, $participant, $project);
+
+                        $em->persist($userWithPosition);
+
+                        $output->writeln(sprintf(
+                            '<comment>User with position for Proejct#<info>%s</info> <info>successfully</info> created</comment>',
+                            $project->getId()
+                        ));
+                    }
+                } else {
+                    $output->writeln(sprintf(
+                        '<comment>Project #<info>%s</info> without participants</comment>',
+                        $project->getId()
+                    ));
+                }
+            }
+        } else {
+            $output->writeln('<comment>There\'s not projects without participants</comment>');
+        }
+
+        $em->flush();
+    }
+
+    /**
+     * Get user with position
+     *
+     * @param UserWithPositionRepository $repository
+     * @param User                       $user
+     * @param Project                    $project
+     *
+     * @return UserWithPosition
+     */
+    private function getUserWithPosition($repository, $user, $project)
+    {
+        $userWithPosition = $repository->findByUserAndProject($user, $project);
+
+        if (null === $userWithPosition) {
+            $userWithPosition = (new UserWithPosition())
+                ->setProject($project)
+                ->setUser($user)
+                ->setPositions($user->getPosition());
+        } else {
+            $userWithPosition->setPositions($user->getPosition());
+        }
+
+        return $userWithPosition;
+    }
+}
--- src/Stfalcon/Bundle/PortfolioBundle/Repository/UserWithPositionRepository.php
+++ src/Stfalcon/Bundle/PortfolioBundle/Repository/UserWithPositionRepository.php
@@ -2,8 +2,10 @@
 
 namespace Stfalcon\Bundle\PortfolioBundle\Repository;
 
+use Application\Bundle\UserBundle\Entity\User;
 use Doctrine\ORM\EntityRepository;
 use Stfalcon\Bundle\PortfolioBundle\Entity\Project;
+use Stfalcon\Bundle\PortfolioBundle\Entity\UserWithPosition;
 
 /**
  * UserWithPositionRepository class
@@ -26,5 +28,27 @@ class UserWithPositionRepository extends EntityRepository
         return $qb->where($qb->expr()->eq('uwp.project', $project->getId()))
                   ->getQuery()
                   ->getResult();
+    }
+
+    /**
+     * Find by user and project
+     *
+     * @param User    $user
+     * @param Project $project
+     *
+     * @return null|UserWithPosition
+     */
+    public function findByUserAndProject($user, $project)
+    {
+        $qb = $this->createQueryBuilder('uwp');
+
+        return $qb->where($qb->expr()->eq('uwp.project', ':project'))
+                  ->andWhere($qb->expr()->eq('uwp.user', ':user'))
+                  ->setParameters([
+                      'project' => $project,
+                      'user'    => $user,
+                  ])
+                  ->getQuery()
+                  ->getOneOrNullResult();
     }
 }
