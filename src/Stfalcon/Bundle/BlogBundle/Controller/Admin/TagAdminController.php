12461114d43d848fc1e58bab1f96d0d47a55c75e
Artem Genvald <genvaldartem@gmail.com>
2016-04-25 23:07:26 +0300

    Fix translations for posts and tags

---
+++ src/Stfalcon/Bundle/BlogBundle/Controller/Admin/TagAdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Stfalcon\Bundle\BlogBundle\Controller\Admin;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+use A2lix\TranslationFormBundle\Annotation\GedmoTranslation;
+
+/**
+ * TagAdminController
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ */
+class TagAdminController extends CRUDController
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @GedmoTranslation
+     */
+    public function createAction()
+    {
+        return parent::createAction();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @GedmoTranslation
+     */
+    public function editAction($id = null)
+    {
+        return parent::editAction($id);
+    }
+}
