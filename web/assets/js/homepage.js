e645907194eecf9ffd32bbbcf20e528c1f3e7108
oleg.demchuk <oleg.demchuk@stfalcon.com>
2016-04-29 11:00:41 +0300

    made new design for main page + customize hire-us popup

--- web/assets/js/homepage.js
+++ web/assets/js/homepage.js
@@ -67,14 +67,14 @@ $(document).ready(function(){
         }
     };
 
-    initMobileSlider();
+    // initMobileSlider();
 
     // Hide mobile navigation
     $(document).on('click', '.close-mobile-nav', function(){
         $('body').removeClass('open-navigation open-main-nav open-languages-nav');
     });
     $(window).bind('resize', function () {
-        initMobileSlider();
+        // initMobileSlider();
         if(window.matchMedia && window.matchMedia('(min-width: 800px)').matches){
             $('body').removeClass('open-navigation open-main-nav open-languages-nav');
         }
@@ -82,142 +82,144 @@ $(document).ready(function(){
 });
 
 
-var $projectList = $('.projects-row'),
-    $projectRow = $('.projects-list'),
-    heightHuck = 0,
-    $prevButton = $('.projects-list .prev-tab'),
-    $nextButton = $('.projects-list .next-tab'),
-    currentTab;
+// var $projectList = $('.projects-row'),
+//     $projectRow = $('.projects-list'),
+//     heightHuck = 0,
+//     $prevButton = $('.projects-list .prev-tab'),
+//     $nextButton = $('.projects-list .next-tab'),
+//     currentTab;
+//
+// function changeServiceCategory(category){
+//     var loadedCount = 0;
+//
+//     $projectRow.html("");
+//     // Build projects items
+//     serviceData[category].projects.forEach(function(item, index) {
+//         var $projectCell = index === 0 ? $('<div class="project-cell project-cell-l"/>') : $('<div class="project-cell"/>');
+//         var $projectImg = $('<img alt="' + item.name +'"/>').on('load', function(){
+//             loadedCount++;
+//         })
+//             .attr('src', item.projectPreviewURL);
+//         var $linkToProject = $('<a href="'+item.URL+'"/>')
+//         var $projectInfo = $('<span class="project-info">' +
+//         '<span class="project-info-text">' +
+//         '<span class="project-name">' + item.name + '</span>' +
+//         '<span class="project-description">' +
+//         item.description +
+//         '</span>' +
+//         '</span>' +
+//         '<span class="helper"></span>' +
+//         '</span>' +
+//         '</span>');
+//
+//         $linkToProject
+//             .append($projectImg)
+//             .append($projectInfo);
+//         $projectCell.append($linkToProject);
+//         $projectRow.append($projectCell);
+//     });
+//
+//     checkButtonsState();
+// };
 
-function changeServiceCategory(category){
-    var loadedCount = 0;
+/*Projects-tabs function on main page*/
 
-    $projectRow.html("");
-    // Build projects items
-    serviceData[category].projects.forEach(function(item, index) {
-        var $projectCell = index === 0 ? $('<div class="project-cell project-cell-l"/>') : $('<div class="project-cell"/>');
-        var $projectImg = $('<img alt="' + item.name +'"/>').on('load', function(){
-            loadedCount++;
-        })
-            .attr('src', item.projectPreviewURL);
-        var $linkToProject = $('<a href="'+item.URL+'"/>')
-        var $projectInfo = $('<span class="project-info">' +
-        '<span class="project-info-text">' +
-        '<span class="project-name">' + item.name + '</span>' +
-        '<span class="project-description">' +
-        item.description +
-        '</span>' +
-        '</span>' +
-        '<span class="helper"></span>' +
-        '</span>' +
-        '</span>');
+// var servicesTabs = $('.projects-tabs a');
+//
+// servicesTabs.on('click', function(e){
+//     var category = $(this).data('category');
+//     if(category) {
+//         var itemIndex = $(e.target).closest('li').index();
+//         if(itemIndex == servicesTabs.length - 2){
+//             /*
+//             * When will created preview for category 'CONSULTING AND AUDIT'
+//             * todo: edit expression in "if" change 2 on 1
+//             * */
+//             $nextButton.addClass('disabled');
+//             $prevButton.removeClass('disabled');
+//         } else if(itemIndex != 0) {
+//             $prevButton.removeClass('disabled');
+//         }
+//         changeServiceCategory(category);
+//         servicesTabs.removeClass('active');
+//         $(this).addClass('active');
+//         currentTab = $(this).closest('li');
+//     }
+// });
 
-        $linkToProject
-            .append($projectImg)
-            .append($projectInfo);
-        $projectCell.append($linkToProject);
-        $projectRow.append($projectCell);
-    });
+// function activeTabByHash(hash) {
+//     var $link = $('.projects-tabs').find('a[data-category="'+hash+'"]');
+//     $link.trigger('click');
+//     changeServiceCategory(hash);
+// }
+//
+// function changeTabByHash(hash) {
+//     if (hash.length>0 && hash != 'undefined') {
+//         activeTabByHash(hash);
+//     } else {
+//         // go to default category
+//         activeTabByHash('development');
+//     }
+// }
 
-    checkButtonsState();
-};
+// function toNextItem(){
+//     var $nextItem = currentTab.nextAll().first();
+//     if($nextItem.length && !$nextItem.hasClass('disabled')) {
+//         var $sliderLink = $nextItem.find('a');
+//         window.location.hash = $sliderLink.attr('href');
+//         $sliderLink.trigger('click');
+//         $(window).trigger("hashchange");
+//     }
+//     checkButtonsState();
+// }
+//
+// function toPrevItem(){
+//     var $prevItem = currentTab.prevAll().first();
+//     if($prevItem.length) {
+//         var $sliderLink = $prevItem.find('a');
+//         window.location.hash = $sliderLink.attr('href');
+//         $sliderLink.trigger('click');
+//         $(window).trigger("hashchange");
+//     }
+//
+//     checkButtonsState();
+// }
 
-var servicesTabs = $('.projects-tabs a');
+// function checkButtonsState(){
+//     switch($(currentTab).index()){
+//         case 0:
+//             $prevButton.addClass('disabled');
+//             $nextButton.removeClass('disabled');
+//             break;
+//         case 1:
+//         case 2:
+//             $prevButton.removeClass('disabled');
+//             $nextButton.removeClass('disabled');
+//             break;
+//         case 3:
+//             $prevButton.removeClass('disabled');
+//             $nextButton.addClass('disabled');
+//     }
+// }
+//
+// $nextButton.on('click', function(){
+//     toNextItem();
+// });
+//
+// $prevButton.on('click', function(){
+//     toPrevItem();
+// });
 
-servicesTabs.on('click', function(e){
-    var category = $(this).data('category');
-    if(category) {
-        var itemIndex = $(e.target).closest('li').index();
-        if(itemIndex == servicesTabs.length - 2){
-            /*
-            * When will created preview for category 'CONSULTING AND AUDIT'
-            * todo: edit expression in "if" change 2 on 1
-            * */
-            $nextButton.addClass('disabled');
-            $prevButton.removeClass('disabled');
-        } else if(itemIndex != 0) {
-            $prevButton.removeClass('disabled');
-        }
-        changeServiceCategory(category);
-        servicesTabs.removeClass('active');
-        $(this).addClass('active');
-        currentTab = $(this).closest('li');
-    }
-});
-
-function activeTabByHash(hash) {
-    var $link = $('.projects-tabs').find('a[data-category="'+hash+'"]');
-    $link.trigger('click');
-    changeServiceCategory(hash);
-}
-
-function changeTabByHash(hash) {
-    if (hash.length>0 && hash != 'undefined') {
-        activeTabByHash(hash);
-    } else {
-        // go to default category
-        activeTabByHash('development');
-    }
-}
-
-function toNextItem(){
-    var $nextItem = currentTab.nextAll().first();
-    if($nextItem.length && !$nextItem.hasClass('disabled')) {
-        var $sliderLink = $nextItem.find('a');
-        window.location.hash = $sliderLink.attr('href');
-        $sliderLink.trigger('click');
-        $(window).trigger("hashchange");
-    }
-    checkButtonsState();
-}
-
-function toPrevItem(){
-    var $prevItem = currentTab.prevAll().first();
-    if($prevItem.length) {
-        var $sliderLink = $prevItem.find('a');
-        window.location.hash = $sliderLink.attr('href');
-        $sliderLink.trigger('click');
-        $(window).trigger("hashchange");
-    }
-
-    checkButtonsState();
-}
-
-function checkButtonsState(){
-    switch($(currentTab).index()){
-        case 0:
-            $prevButton.addClass('disabled');
-            $nextButton.removeClass('disabled');
-            break;
-        case 1:
-        case 2:
-            $prevButton.removeClass('disabled');
-            $nextButton.removeClass('disabled');
-            break;
-        case 3:
-            $prevButton.removeClass('disabled');
-            $nextButton.addClass('disabled');
-    }
-}
-
-$nextButton.on('click', function(){
-    toNextItem();
-});
-
-$prevButton.on('click', function(){
-    toPrevItem();
-});
-
-$(document).ready(function() {
-    var hash = window.location.hash;
-    changeTabByHash(hash);
-});
-
-
-$.History.bind(function(state) {
-    hash = state;
-    changeTabByHash(hash);
-});
+// $(document).ready(function() {
+//     var hash = window.location.hash;
+//     changeTabByHash(hash);
+// });
+//
+//
+// $.History.bind(function(state) {
+//     hash = state;
+//     changeTabByHash(hash);
+// });
 
 $(document).on('click', '.show-more-info', function(){
    $(this).hide(200);
