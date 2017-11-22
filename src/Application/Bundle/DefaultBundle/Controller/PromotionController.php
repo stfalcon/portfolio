d3389f68faaaee3a5d0ff9b2d961758bc1637278
Andrew.Tom <andij.tomchyshyn@stfalcon.com>
2017-11-22 09:41:50 +0200

    re #35703  fix show pdf

--- src/Application/Bundle/DefaultBundle/Controller/PromotionController.php
+++ src/Application/Bundle/DefaultBundle/Controller/PromotionController.php
@@ -8,11 +8,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Application\Bundle\DefaultBundle\Form\Type\PromotionOrderFormType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 /**
  * Promotion controller. For promotions actions
@@ -110,5 +112,24 @@ class PromotionController extends Controller
         } else {
             return new JsonResponse(['status' => 'error']);
         }
+    }
+
+    /**
+     * @param string $pdfFilename
+     *
+     * @return BinaryFileResponse
+     *
+     * @Route("/show_pdf/{pdfFilename}", name="show_pdf")
+     */
+    public function showPdfAction($pdfFilename)
+    {
+        $path = $this->getParameter('kernel.root_dir').'/../web/pdf/'.$pdfFilename;
+
+        $response = new BinaryFileResponse($path);
+
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, $pdfFilename);
+
+        return $response;
     }
 }
