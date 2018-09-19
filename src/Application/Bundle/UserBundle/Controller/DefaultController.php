340174b49ef8e051761d4a757f97673261594248
tomchyshyn <andrij.tomchyshyn@stfalcon.com>
2018-09-19 12:37:38 +0300

    Add user start and end fields.
    re #45904

--- src/Application/Bundle/UserBundle/Controller/DefaultController.php
+++ src/Application/Bundle/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Default controller.
@@ -20,15 +21,25 @@ class DefaultController extends Controller
      *
      * @param Request $request Request
      *
-     * @return array()
+     * @return Response
+     *
      * @Route("/team", name="team")
-     * @Template()
      */
     public function indexAction(Request $request)
     {
+//        $users = $this->getDoctrine()
+//            ->getRepository('ApplicationUserBundle:User')
+//            ->findAllActiveUsers();
         $users = $this->getDoctrine()
-                      ->getRepository('ApplicationUserBundle:User')
-                      ->findAllActiveUsers();
+            ->getRepository('ApplicationUserBundle:User')->findBy(
+                [
+                    'hideFromTeamSection' => false,
+                    'locked' => false,
+                ],
+                [
+                    'ordering' => 'ASC',
+                ]
+            );
 
         $interestsList = User::getInterestsList();
 
@@ -36,9 +47,9 @@ class DefaultController extends Controller
         $seo->addMeta('property', 'og:url', $this->generateUrl($request->get('_route'), [], true))
             ->addMeta('property', 'og:type', SeoOpenGraphEnum::WEBSITE);
 
-        return [
+        return $this->render('@ApplicationUser/Default/index.html.twig', [
             'users'         => $users,
             'interestsList' => $interestsList,
-        ];
+        ]);
     }
 }
